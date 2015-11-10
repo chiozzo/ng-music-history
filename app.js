b74b084078415bb0e09a1d6494e170eb60ddbc49
mncross <mncross@gmail.com>
2015-11-10 14:30:43 -0600

    having trouble with ng-repeat

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 var app = angular.module('musicHistory', []);
 
 console.log("hello");
+
+app.controller('songsCtrl', function($scope){
+
+	$scope.songs = null;
+
+	$.ajax('songs1.json')
+	.done(function(songsJSON){
+		songs = songsJSON;
+		console.log("songs", songs);
+	}).fail(function(error){
+		console.log("error", error);
+	});
+
+});
