d7eaefbe66efe2fc80b332eaa536625593f58204
mncross <mncross@gmail.com>
2015-11-10 22:11:43 -0600

    ajax call to json working; MVP styling

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var app = angular.module('musicHistory', []);
 
-console.log("hello");
+app.controller("songsCtrl", function($scope, $http) {
 
-app.controller('songsCtrl', function($scope){
+  $scope.songs = null;
 
-	$scope.songs = null;
+  $.ajax('songs1.json')
+  .done(function(ajaxData){
+    $scope.songs = ajaxData;
+    $scope.$apply();
+  }).fail(function(error){
+    console.log("error", error);
+  });
 
-	$.ajax('songs1.json')
-	.done(function(songsJSON){
-		songs = songsJSON;
-		console.log("songs", songs);
-	}).fail(function(error){
-		console.log("error", error);
-	});
+  //Still null because $http is asynrounous and is still running
+  // console.log("$scope.songs", $scope.songs);
+/**************************************************
+  $scope.deleteSong() = function(i){
+    console.log("hello");
+  }
+
+**************************************************/
 
 });
+
+// $.material.init();
